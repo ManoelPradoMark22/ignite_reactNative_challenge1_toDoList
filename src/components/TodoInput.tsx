760e2061de947bb21823e06fecc63eb328636b0c
unknown <manoelprado.aecjr@gmail.com>
2021-10-15 13:56:31 -0300

    adding taks and listing

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -7,10 +7,13 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
-  function handleAddNewTask() {
-    //TODO - Call addTask if task not empty and clean input value 
+  function handleAddNewTask() { 
+    if (task!==''){
+      addTask(task);
+      setTask('');
+    }
   }
 
   return (
@@ -21,13 +24,15 @@ export function TodoInput({ addTask }: TodoInputProps) {
         placeholderTextColor="#B2B2B2"
         returnKeyType="send"
         selectionColor="#666666"
-        //TODO - use value, onChangeText and onSubmitEditing props
+        onSubmitEditing={handleAddNewTask}
+        value={task}
+        onChangeText={setTask}
       />
       <TouchableOpacity
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
-        //TODO - onPress prop
+        onPress={handleAddNewTask}
       >
         <Icon name="chevron-right" size={24} color="#B2B2B2" />
       </TouchableOpacity>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,13 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    };
+
+    setTasks(oldState => [...oldState, task]);
   }
 
   function handleToggleTaskDone(id: number) {
